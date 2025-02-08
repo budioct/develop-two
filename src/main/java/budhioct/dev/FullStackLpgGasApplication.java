f7ab39ac26da1599b3d453aa807913d56b239cad
Budhi Octaviansyah <budioct@gmail.com>
2025-02-08 19:43:20 +0700

    start project

---
+++ src/main/java/budhioct/dev/FullStackLpgGasApplication.java
@@ -0,0 +1,13 @@
+package budhioct.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackLpgGasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackLpgGasApplication.class, args);
+	}
+
+}
