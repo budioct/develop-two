6c771273010275cd5bc32a809a659ece1b812d69
Budhi Octaviansyah <budioct@gmail.com>
2025-02-14 13:42:09 +0700

    add enum

---
+++ src/main/java/budhioct/dev/utilities/Ownership.java
@@ -0,0 +1,10 @@
+package budhioct.dev.utilities;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Ownership {
+    PERTAMINA,
+    OFFICIAL_AGENT,
+    SUB_AGENT
+}
