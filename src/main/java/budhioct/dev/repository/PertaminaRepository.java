62f33d44cbfe4af2dce69cf20ce5c48785dc96f8
Budhi Octaviansyah <budioct@gmail.com>
2025-03-01 12:44:24 +0700

    update

--- src/main/java/budhioct/dev/repository/PertaminaRepository.java
+++
@@ -1,7 +0,0 @@
-package budhioct.dev.repository;
-
-import budhioct.dev.entity.Pertamina;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PertaminaRepository extends JpaRepository<Pertamina, Long> {
-}
---
+++ src/main/java/budhioct/dev/repository/PutanginaRepository.java
@@ -0,0 +1,7 @@
+package budhioct.dev.repository;
+
+import budhioct.dev.entity.Putangina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PutanginaRepository extends JpaRepository<Putangina, Long> {
+}
