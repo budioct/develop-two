9c6cea9a0d1123c2749261e36e7dbce801d616c7
Budhi Octaviansyah <budioct@gmail.com>
2025-03-03 21:59:32 +0700

    update

--- src/main/java/budhioct/dev/security/auditing/ApplicationAuditAware.java
+++ src/main/java/budhioct/dev/security/auditing/ApplicationAuditAware.java
@@ -1,6 +1,10 @@
 package budhioct.dev.security.auditing;
 
+import budhioct.dev.security.data.User;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -8,6 +12,16 @@ public class ApplicationAuditAware implements AuditorAware<Long> {
 
     @Override
     public Optional<Long> getCurrentAuditor() {
-        return Optional.empty();
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) authentication.getPrincipal();
+
+        return Optional.ofNullable(userPrincipal.getId());
     }
 }
