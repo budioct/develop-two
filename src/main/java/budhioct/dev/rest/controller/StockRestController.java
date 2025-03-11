c06df1c0562194c9a73fc7972423c9687f2a2152
Budhi Octaviansyah <budioct@gmail.com>
2025-03-11 16:16:31 +0700

    update

--- src/main/java/budhioct/dev/rest/controller/StockRestController.java
+++ src/main/java/budhioct/dev/rest/controller/StockRestController.java
@@ -6,6 +6,7 @@ import budhioct.dev.service.StockService;
 import budhioct.dev.utilities.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/stock")
+@PreAuthorize("hasAnyRole('USER')")
 public class StockRestController {
 
     @Autowired
@@ -22,6 +24,7 @@ public class StockRestController {
             path = "/{id}/monitoring-stock",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @PreAuthorize("hasAnyAuthority('user:read')")
     public RestResponse.object<StockDTO.StockResponse> monitorStockLevels(@PathVariable(name = "id") Long id) {
         StockDTO.StockResponse stock = stockService.monitorStockLevels(id);
         return RestResponse.object.<StockDTO.StockResponse>builder()
