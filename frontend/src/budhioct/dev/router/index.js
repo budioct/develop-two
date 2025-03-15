21c2acdda8c436cbd88de8189ce6adbde1224362
Budhi Octaviansyah <budioct@gmail.com>
2025-03-15 10:55:43 +0700

    update

--- frontend/src/budhioct/dev/router/index.js
+++ frontend/src/budhioct/dev/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
+import {useAuthStore} from "../stores/authStore.js";
 import Pages from "../views/pages/Pages.vue";
 import Home from "../views/pages/Home.vue";
+import Nprogress from "nprogress";
 
 const routes = [
     {
@@ -16,11 +18,13 @@ const routes = [
               path: '/home',
               name: 'home',
               component: Home,
+              meta: {requiresAuth: true}
             },
             {
                 path: '/warung',
                 name: 'warung',
                 component: () => import('../views/pages/Warung.vue'),
+                meta: {requiresAuth: true}
             },
         ]
     },
@@ -28,15 +32,39 @@ const routes = [
         path: '/login',
         name: 'login',
         component: () => import('../views/pages/auth/Login.vue'),
+        meta: {guest: true},
     },
     {
         path: '/register',
         name: 'register',
         component: () => import('../views/pages/auth/Register.vue'),
+        meta: {guest: true},
     },
 ]
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: routes
+});
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+        //console.info('Access denied: You must be logged in to access this page.');
+        return next({name: 'login'});
+    }
+
+    if (to.meta.guest && authStore.isAuthenticated) {
+        //console.info('Access denied: You are already logged in.');
+        return next({name: 'home'});
+    }
+
+    Nprogress.start();
+    next();
+
+});
+
+router.afterEach(() => {
+    Nprogress.done();
 });
