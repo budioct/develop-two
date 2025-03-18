1ef29177a18f07404fb929e859922cac2fd51741
Budhi Octaviansyah <budioct@gmail.com>
2025-03-18 11:55:29 +0700

    update

--- frontend/src/budhioct/dev/services/apiService.js
+++ frontend/src/budhioct/dev/services/apiService.js
@@ -1,5 +1,6 @@
 import {urlApi} from "../constants/urlApi.js";
 import axios from 'axios';
+import {useAuthStore} from "../stores/authStore.js";
 
 const register = async (dataRegister) => {
     try {
@@ -23,7 +24,23 @@ const login = async (dataLogin) => {
     }
 };
 
+const logout = async () => {
+    try {
+        return await axios.post(urlApi.auth.logout, {}, {
+            withCredentials: true,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${useAuthStore().token}`,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        throw error
+    }
+};
+
 export {
     register,
     login,
+    logout,
 }
