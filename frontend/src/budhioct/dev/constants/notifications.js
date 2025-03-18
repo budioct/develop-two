41f365f8897a4d428cb71fce79adff4f63653030
Budhi Octaviansyah <budioct@gmail.com>
2025-03-18 11:54:58 +0700

    update

--- frontend/src/budhioct/dev/constants/notifications.js
+++ frontend/src/budhioct/dev/constants/notifications.js
@@ -6,28 +6,28 @@ export const useNotification = {
     success(title, message) {
         toast.success(`<b>${title}:</b> ${message}`, {
             position: "top",
-            duration: 8000,
+            duration: 9000,
             dismissible: true,
         });
     },
     error(title, message) {
         toast.error(`<b>${title}:</b> ${message}`, {
             position: "top",
-            duration: 8000,
+            duration: 9000,
             dismissible: true,
         });
     },
     info(title, message) {
         toast.info(`<b>${title}:</b> ${message}`, {
             position: "top",
-            duration: 8000,
+            duration: 9000,
             dismissible: true,
         });
     },
     warning(title, message) {
         toast.warning(`<b>${title}:</b> ${message}`, {
             position: "top",
-            duration: 8000,
+            duration: 9000,
             dismissible: true,
         });
     },
