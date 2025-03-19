cab2a60dee08f1a8e95a728b38ca40a4d8d4005c
Budhi Octaviansyah <budioct@gmail.com>
2025-03-19 11:58:10 +0700

    update

---
+++ frontend/src/budhioct/dev/components/Hero.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container py-5">
+    <header class="mb-1 border-bottom">
+      <div>
+        <span class="fs-4">Profil</span>
+      </div>
+    </header>
+
+    <div class="p-3 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h2 class="display-7 fw-bold"><span class="dejum">GASKITA</span></h2>
+        <p class="col-md-8 fs-5 justify-content-between">
+          Dari Elpiji 3 kg sampai Bright Gas, Kami Bikin Hidup Anda Terang!
+        </p>
+
+        <div class="p-3">
+          <div>
+            <h4 class="display-7 fw-bold"><span class="dejum">Tentang Kami</span></h4>
+            <p class="col-md-8  justify-text">
+              Kami adalah <span class="dejum">GASKITA</span>, perusahaan fiktif yang selalu <strong>nge-gas</strong> dalam menyediakan
+              gas untuk kebutuhan rumah tangga Anda.
+              Dari Elpiji 3 kg yang bersubsidi sampai Bright Gas yang bikin dapur Anda <strong>terang benderang</strong>,
+                kami siap melayani dengan senyuman meskipun kadang agak kecut.
+            </p>
+          </div>
+          <div>
+            <h4 class="display-7 fw-bold"><span class="dejum">Visi Misi</span></h4>
+            <p class="col-md-8 fs-5 justify-text">
+              <ol>
+                <li>Menyediakan gas bersubsidi yang terjangkau, walaupun kadang bikin deg-degan karena stoknya langka.</li>
+                <li>Menawarkan gas non-subsidi yang bikin hidup Anda lebih terang.</li>
+                <li>Menjaga hubungan baik dengan masyarakat, meskipun kadang bikin gregetan.</li>
+              </ol>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.py-5 {
+  padding-top: 5px !important;
+  padding-bottom: 2px !important;
+}
+
+.justify-text {
+  text-align: justify;
+}
+</style>
