fe7d2641b0b066d850e82562c37776df94b3c629
Budhi Octaviansyah <budioct@gmail.com>
2025-03-20 17:32:52 +0700

    update

---
+++ frontend/src/budhioct/dev/views/pages/business/OfficialAgents.vue
@@ -0,0 +1,74 @@
+<script setup>
+import {onMounted, ref} from 'vue';
+import {listOfficialAgent} from "../../../services/apiService.js";
+import KTDatatable from "../../../components/tables/KTDatatable.vue";
+
+const officialAgents = ref([]);
+const countPage = ref(10);
+const isLoading = ref(true); // Tambahkan state loading
+const columns = ref([
+  {key: 'no', label: 'No'},
+  {key: 'agentName', label: 'Official Agent'},
+  {key: 'subholdingGroupAffiliate', label: 'Subholding'},
+  {key: 'address', label: 'Address'},
+  {key: 'stock_amount_gas', label: 'Gas Stock Amount'},
+]);
+
+function setOfficialAgents(data) {
+  officialAgents.value = data
+      .sort((a, b) => b.createdAt - a.createdAt)
+      .map((item, index) => ({
+        no: index + 1,
+        id: item.id,
+        agentName: item.agentName,
+        address: item.address,
+        stock_amount_gas: item.stock_amount_gas,
+        subholdingGroupAffiliate: item.subholdingGroupAffiliate,
+        createdAt: item.createdAt,
+        updatedAt: item.updatedAt
+      }));
+}
+
+async function fetchOfficialAgents() {
+  try {
+    const response = await listOfficialAgent();
+    if (response.status === 200) {
+      setOfficialAgents(response.data.officialAgent);
+    }
+  } catch (error) {
+    console.error('Error fetching stakeholder data:', error);
+  } finally {
+    isLoading.value = false; // Hentikan loading setelah data diambil
+  }
+}
+
+onMounted(async () => {
+  await fetchOfficialAgents();
+});
+
+</script>
+
+<template>
+  <div class="container py-5">
+    <header class="mb-1 border-bottom">
+      <div>
+        <span class="fs-4">Official Agents</span>
+      </div>
+    </header>
+
+    <!-- Tampilkan KTDatatable -->
+    <KTDatatable :columns="columns" :data="officialAgents" :perPage="countPage" :loading="isLoading">
+      <template #actions="{ row }">
+        <button class="btn btn-outline-primary btn-sm" @click="">Detail</button>
+      </template>
+    </KTDatatable>
+
+  </div>
+</template>
+
+<style scoped>
+.py-5 {
+  padding-top: 5px !important;
+  padding-bottom: 2px !important;
+}
+</style>
