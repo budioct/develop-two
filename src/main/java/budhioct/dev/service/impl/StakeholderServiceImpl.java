56e37e918debc0fd64e1279b1ea396abd870732d
Budhi Octaviansyah <budioct@gmail.com>
2025-04-21 10:30:07 +0700

    update

--- src/main/java/budhioct/dev/service/impl/StakeholderServiceImpl.java
+++ src/main/java/budhioct/dev/service/impl/StakeholderServiceImpl.java
@@ -1,7 +1,10 @@
 package budhioct.dev.service.impl;
 
 import budhioct.dev.dto.StakeholderDTO;
+import budhioct.dev.entity.LogStock;
 import budhioct.dev.entity.Stakeholder;
+import budhioct.dev.entity.Stock;
+import budhioct.dev.repository.LogStockRepository;
 import budhioct.dev.repository.StakeholderRepository;
 import budhioct.dev.service.StakeholderService;
 import budhioct.dev.utilities.ValidationService;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,6 +22,7 @@ import java.util.List;
 public class StakeholderServiceImpl implements StakeholderService {
 
     private final StakeholderRepository stakeholderRepository;
+    private final LogStockRepository logStockRepository;
     private final ValidationService validation;
 
     @Transactional(readOnly = true)
@@ -40,6 +45,34 @@ public class StakeholderServiceImpl implements StakeholderService {
         Stakeholder stakeholder = stakeholderRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "stakeholder not found"));
         return StakeholderDTO.toStakeholderDetailResponse(stakeholder);
+    }
+
+    @Transactional
+    public StakeholderDTO.StakeholderDetailResponse reProductionStock(Long id) {
+        validation.validate(id);
+        Stakeholder stakeholder = stakeholderRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "stakeholder not found"));
+
+        if (stakeholder.getStock().getStock_amount() > 10000L) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "stakeholder stock must be less than or equal to 10000");
+        }
+
+        Stock stock = stakeholder.getStock();
+        Long reproduceAmount = 30000L;
+        stock.setStock_amount(stock.getStock_amount() + reproduceAmount);
+
+        logStockChange(stock, reproduceAmount, "RE-PRODUCTION");
+
+        return StakeholderDTO.toStakeholderDetailResponse(stakeholder);
+    }
+
+    private void logStockChange(Stock stock, Long amount, String status) {
+        LogStock logStock = new LogStock();
+        logStock.setStock(stock);
+        logStock.setNumberOfChanges(amount);
+        logStock.setStatus(status);
+        logStock.setDate(LocalDateTime.now());
+        logStockRepository.save(logStock);
     }
 
 }
